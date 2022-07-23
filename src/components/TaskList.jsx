139946beae3893b894347f712c1bc2f39983a36c
nnnnadia <tristaonadia@gmail.com>
2022-07-23 13:35:58 -0300

    Implementação do botão de deletar tarefa.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,38 @@
-import { List, ListItemButton, ListItemText } from '@mui/material';
 import React, { useState, useContext } from 'react';
+import { List, ListItem, ListItemButton, IconButton, ListItemText } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import TaskContext from '../context';
 
 function TaskList() {
   const [selectedTask, setSelectedTask] = useState(null);
-  const { tasks } = useContext(TaskContext);
+  const { tasks, setTasks } = useContext(TaskContext);
+
+  const handleDelete = (indexToBeDeleted) => {
+    const updatedTasks = tasks.filter((_task, index) => index !== indexToBeDeleted);
+    setTasks(updatedTasks);
+  };
 
   return (
     <List>
       { tasks.map((task, index) => (
-        <ListItemButton
+        <ListItem
           key={ index }
-          selected={ selectedTask === index }
-          onClick={ () => setSelectedTask(index) }
+          secondaryAction={
+            <div hidden={ selectedTask !== index }>
+              <IconButton onClick={ () => handleDelete(index) }>
+                <DeleteForeverIcon />
+              </IconButton>
+            </div>
+          }
+          disablePadding
         >
-          <ListItemText primary={ task } />
-        </ListItemButton>
+          <ListItemButton
+            selected={ selectedTask === index }
+            onClick={ () => setSelectedTask(index) }
+          >
+            <ListItemText primary={ task } />
+          </ListItemButton>
+        </ListItem>
       )) }
     </List>
   );
